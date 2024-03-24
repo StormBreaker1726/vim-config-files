9c0b57237413942c6b2690203bfede630eb5a309
João Víctor Oliveira <joaovictoroliveira1452@gmail.com>
2024-03-24 22:17:43 +0000

    feat: defines new keyboard shortcuts

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,9 @@ vim.cmd("set tabstop=4")        -- Number of spaces a tab character counts for
 vim.cmd("set softtabstop=4")    -- Number of spaces inserted for a tab in insert mode
 vim.cmd("set shiftwidth=4")     -- Number of spaces to use for auto-indent
 vim.g.mapleader = " "
-
+-- Atalhos para mover entre janelas usando leader key
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
 
